36785e6b28488a8b27b0dccc1a2fca94a59ef639
CoryCMyers <CoryCMyers@users.noreply.github.com>
2021-10-17 21:16:16 -0400

    Adding PyPoll.py File

--- PyPoll.py
+++ PyPoll.py
@@ -1,9 +1,3 @@
-# Total number of votes cast
-# A complete list of candidates who received votes
-# Total number of votes each candidate received
-# Percentage of votes each candidate won
-# The winner of the election based on popular vote 
-
 # Add our dependencies.
 import csv
 import os
@@ -12,39 +6,19 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+total_votes = 0
+
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-    # Read and print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
 
-    # Print Each Row in th CSV File
+    # Print each row in the CSV file.
     for row in file_reader:
-        print(row)
+            # Add to the total vote count
+            total_votes = total_votes + 1
 
-# Print the file object.
-print(election_results)
-
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("Analysis", "election_analysis.txt")
-# Using the open() function with the "w" mode we will write data to the file.
-open(file_to_save, "w")
-
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
-
-    # Write some data to the file.
-    txt_file.write("Counties in the Election")
-    txt_file.write("\n-------------------------")
-    txt_file.write("\nArapahoe, ")
-    txt_file.write("\nDenver, ")
-    txt_file.write("\nJefferson")
-
-    # Write three counties to the file.
-    #\n to start on Next Line
-    #txt_file.write("Arapahoe, \nDenver, \nJefferson") """
+    print(total_votes)
